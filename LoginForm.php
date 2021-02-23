35e2700fa870343bc643c71574fa59fe9d833123
ShivaniGoyal23 <shinugoyal12@gmail.com>
2021-02-23 14:49:34 +0530

    Created alert for login failure

--- LoginForm.php
+++ LoginForm.php
@@ -28,38 +28,6 @@
   } 
 ?>
 <!DOCTYPE html>
-=======
-<?php
-   include("config.php");
-   session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: welcome.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
-   }
-?>
-
->>>>>>> b007053f11fb648fc5995682f41e96200e24c3c4:Login/LoginForm.html
 <html lang="en">
 <head>
     <meta charset="UTF-8">
